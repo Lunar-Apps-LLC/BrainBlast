7b85a87181f61206aca4efdc1ac980b8db6f582f
Andrew <drewgarcia23@gmail.com>
2025-03-24 08:37:41 -0500

    Initial Commit

---
+++ BrainBlast/BrainBlastApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainBlastApp.swift
+//  BrainBlast
+//
+//  Created by Andrew Garcia on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BrainBlastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
